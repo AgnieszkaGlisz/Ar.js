356f1af83733e2d941ad6b7df010f99093f2395c
AgnieszkaGlisz <44533570+AgnieszkaGlisz@users.noreply.github.com>
2021-11-24 14:18:57 +0100

    Adding code

---
+++ testMerryChristmas.php
@@ -0,0 +1,48 @@
+<html>
+  <head>
+  <script src="https://aframe.io/releases/1.0.4/aframe.min.js"></script>
+  <script src="https://raw.githack.com/AR-js-org/AR.js/master/aframe/build/aframe-ar.js"></script>
+  </head>
+
+  <body style='margin : 0px; overflow: hidden;' >
+    
+  <a-scene embedded arjs>
+ 
+    <!-- 
+    <a-entity 
+      position="0 0 -50" 
+      scale="0.1 0.1 0.1"
+      gltf-model="https://raw.githubusercontent.com/AgnieszkaGlisz/Ar.js/main/model/iPod.gltf">
+    </a-entity>
+    -->
+    <a-marker type='pattern' url='https://raw.githubusercontent.com/AgnieszkaGlisz/Ar.js/main/hiro.patt'>
+      <a-entity 
+        position="0 0 0" 
+        scale="1 1 1"
+        gltf-model="https://raw.githubusercontent.com/AgnieszkaGlisz/Ar.js/main/tree/XMas_Flint_01.glb"
+      </a-entity>
+    </a-marker>
+      <a-entity camera></a-entity>
+    </a-scene>
+  </body>
+</html>
+
+<!--
+  background-color: #92a8d1;
+embedded arjs='sourceType: webcam;'
+ <a-marker type='pattern' patternUrl='./tree.patt'>
+
+      <a-entity
+          gltf-model="./scene.gltf"
+        ></a-entity>
+      
+    </a-marker>
+
+    <a-entity camera></a-entity>
+
+ <script src="https://rawgit.com/donmccurdy/aframe-extras/master/dist/aframe-extras.loaders.min.js"></script>
+    <script src="https://jeromeetienne.github.io/AR.js/aframe/build/aframe-ar.js"></script>
+
+
+
+  -->
